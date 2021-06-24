eeaa56bee1f56c3fbcb0c7ac2e1cfd53860bf259
Lena <elena.hlihan@gmail.com>
2021-06-24 22:13:06 +0300

    change baseUrl

--- phonebook/src/redux/auth/auth-operations.js
+++ phonebook/src/redux/auth/auth-operations.js
@@ -17,8 +17,8 @@ import {
   verifyError,
 } from './auth-actions';
 
-// axios.defaults.baseURL = 'https://my-phonebook-api.herokuapp.com/api';
-axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = 'https://my-phonebook-api.herokuapp.com/api';
+// axios.defaults.baseURL = 'http://localhost:3000/api';
 
 const token = {
   set(token) {
